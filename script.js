fcf23faa454a56ad447d7b4436cf3140add7bd8a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-16 23:15:37 +0200

    nice effect working

--- script.js
+++ script.js
@@ -15,21 +15,25 @@ function setup() {
 }
 
 function draw() {
-  const tileSize = 200;
+  background('#e84e3c');
+  const tileSize = 50;
 
-  for (let x = 0; x < 3; x++) {
-    for (let y = 0; y < 3; y++) {
-      copy(
-        graphic,
-        0,
-        0,
-        600,
-        600,
-        x * tileSize,
-        y * tileSize,
-        tileSize,
-        tileSize
-      );
+  for (let x = 0; x < 12; x++) {
+    for (let y = 0; y < 12; y++) {
+      const distortion = sin(frameCount * 0.05 + x * 0.5 + y * 0.3) * 50;
+      //source
+      const sx = x * tileSize + distortion;
+      const sy = y * tileSize + distortion;
+      const sh = tileSize + distortion;
+      const sw = tileSize;
+
+      // destination
+      const dx = x * tileSize;
+      const dy = y * tileSize;
+      const dw = tileSize;
+      const dh = tileSize;
+
+      image(graphic, dx, dy, dw, dh, sx, sy, sw, sh);
     }
   }
 }
